954359a6e01c283b1969d56fbf8b061c83dfe747
kwin555 <ktn555@att.net>
2018-02-23 21:16:12 +0000

    validates images

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -148,16 +148,21 @@ RSpec.describe GramsController, type: :controller do
       expect(response).to redirect_to new_user_session_path
     end
 
-    it 'should successfully create a new gram in our database' do
-      user = FactoryBot.create(:user)
-      sign_in user
+  it "should successfully create a new gram in our database" do
+    user = FactoryBot.create(:user)
+    sign_in user
 
-      post :create, params: { gram: { message: 'Hello!' } }
-      expect(response).to redirect_to root_path
-
-      gram = Gram.last
-      expect(gram.message).to eq("Hello!")
-    end
+    post :create, params: {
+      gram: {
+        message: 'Hello!',
+        picture: fixture_file_upload("/picture.png", 'image/png')
+      }
+    }
+    expect(response).to redirect_to root_path
+    gram = Gram.last
+    expect(gram.message).to eq("Hello!")
+    expect(gram.user).to eq(user)
+  end
 
     it 'should properly deal with validation errors' do
       user = FactoryBot.create(:user)
